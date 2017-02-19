4b37ef9a8e4d3cb5d22b383f2879be8bafc8b414
volpe28v <naoki.koda@gmail.com>
2017-02-19 23:36:00 +0900

    add Edit menu

--- src/app/javascripts/main/app-menu.js
+++ src/app/javascripts/main/app-menu.js
@@ -2,27 +2,32 @@ const {app, Menu, MenuItem} = require('electron');
 
 module.exports = class AppMenu {
   static setup(){
-    const settingMenu = new MenuItem({
-      label: 'Setting',
-      submenu: [
-        {
-          label: 'Setting',
-          click() {
-            app.emit('showSetting');
-          }
-        }
-      ]
-    });
-
     const template = AppMenu.getBasicTemplate();
     const menu = Menu.buildFromTemplate(template);
-    menu.append(settingMenu);
     Menu.setApplicationMenu(menu);
   }
 
   static getBasicTemplate() {
-    const template = [];
+    var template = [{
+      label: "Application",
+      submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { label: "Setting", click() { app.emit('showSetting'); }},
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+      ]}, {
+        label: "Edit",
+        submenu: [
+          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+          { type: "separator" },
+          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
 
     return template;
-  }
+	}
 };
