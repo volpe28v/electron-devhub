e475ef90e0e98836d5faf059ab9523a10c8be174
volpe28v <naoki.koda@gmail.com>
2017-02-19 14:56:18 +0900

    URL を config ファイルに保存できるようにした

--- src/app/javascripts/renderer/setting.js
+++ src/app/javascripts/renderer/setting.js
@@ -1,17 +1,35 @@
 const ko = require('knockout');
+const storage = require('electron-json-storage');
 const {ipcRenderer} = require('electron');
 
 class SettingViewModel {
   constructor() {
     this.url = ko.observable();
+    var self = this;
 
-    // 初期表示
-    this.url('http://dev-hub.herokuapp.com/');
+		storage.get('config', function (error, data) {
+			if (error) throw error;
+
+      // 初期表示
+      if (data.url != null){
+        self.url(data.url);
+      }else{
+        self.url('http://dev-hub.herokuapp.com/');
+      }
+		});
   }
 
   setUrl() {
     ipcRenderer.send("setUrl", this.url());
-  }
+
+    // 設定ファイルへ保存
+		var json = {
+			url: this.url()
+		};
+		storage.set('config', json, function (error) {
+			if (error) throw error;
+		});
+	}
 }
 
 window.onload = () => {
